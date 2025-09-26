db94f09b5711b3059369d7c579b562114ec96230
KmiloVm <kmilo7v@gmail.com>
2025-09-25 19:55:51 -0500

    feat: add PATCH and DELETE endpoints for user management by email

--- app.js
+++ app.js
@@ -1,42 +1,64 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-require("dotenv").config()
-const User = require("./models/user")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const User = require("./models/user");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("Connect Database"))
-.catch(error => console.error("error:", error))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connect Database"))
+  .catch((error) => console.error("error:", error));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Server running</h1>")
-})
+  res.send("<h1>Server running</h1>");
+});
 
 app.get("/api/users", async (req, res) => {
   try {
-    const response = await User.find()
-    res.status(200).json(response)
+    const response = await User.find();
+    res.status(200).json(response);
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message });
   }
-})
+});
 
 app.post("/api/users", async (req, res) => {
   try {
-    const newUser = new User(req.body)
-    const response = await newUser.save()
-      res.status(201).json(response)
+    const newUser = new User(req.body);
+    const response = await newUser.save();
+    res.status(201).json(response);
   } catch (error) {
-    res.status(400).json({ error: error.message })
+    res.status(400).json({ error: error.message });
   }
-})
+});
 
-const port = process.env.PORT || 3000
+app.patch("/api/users/:email", async (req, res) => {
+  try {
+    const email = req.params;
+    const updateUser = req.body;
+    await User.updateOne(email, updateUser)
+    res.status(200).json({message: "user update successful"});
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.delete("/api/users/:email", async (req, res) => {
+  try {
+    const email = req.params;
+    await User.deleteOne(email);
+    res.status(200).json({message: "user delete successful"});
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log("Server conect in port: ", port)
-})
+  console.log("Server conect in port: ", port);
+});
